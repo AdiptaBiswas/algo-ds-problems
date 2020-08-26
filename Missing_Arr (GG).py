ec1b5027a8aa8a4a4b235471e9e845e86e326f5a
Adipta Biswas <40853486+AdiptaBiswas@users.noreply.github.com>
2020-08-26 23:20:19 +0530

    Update Missing_Arr (GG).py

--- Missing_Arr (GG).py
+++ Missing_Arr (GG).py
@@ -2,16 +2,14 @@
 # coding: utf-8
 
 # <b>Search Algorithm O(n):</b> <i>Missing Array </i>
-# 
+
 # <b>Problem Source:</b>  https://practice.geeksforgeeks.org/problems/missing-number-in-array/0#ExpectOP
 
-# In[ ]:
-
+# Complexity: O(n)
 
 def Missing(Arr,N):
     print("{}".format(sum(list(range(1,N+1))) - sum(Arr[:])))
-
-    
+  
 T = int(input())
 for i in range(T):
     N = int(input())
