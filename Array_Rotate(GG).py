f1f5b2fcf2c8a3691f01224ebc8fdb6a46ff041c
Adipta Biswas <40853486+AdiptaBiswas@users.noreply.github.com>
2020-08-30 23:26:13 +0530

    Update and rename Array_Rotate.py.py to Array_Rotate(GG).py

--- Array_Rotate(GG).py
+++ Array_Rotate(GG).py
@@ -5,9 +5,6 @@
 # 
 # <b>Problem Source:</b>  https://practice.geeksforgeeks.org/problems/rotate-array-by-n-elements/0
 
-# In[1]:
-
-
 # n is the array and k is the number of positions to shift
 
 def arr_rot(n, k):
@@ -18,19 +15,11 @@ def arr_rot(n, k):
         counter += 1
     return n
 
-
-# In[2]:
-
-
 # Inputs
 
 a = [2, 4, 6, 8, 10, 12, 14, 16, 18, 20]
 shifts = 3
 print(arr_rot(a,shifts))
 
-
-# In[ ]:
-
-
-
+# [8, 10, 12, 14, 16, 18, 20, 2, 4, 6]
 
